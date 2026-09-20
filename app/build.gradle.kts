af9ba84fc5b50a4c457cf6b98ba1fab3dd2ec3a2
Mohamed Sayed <mo01115285816@gmail.com>
2026-09-20 21:19:24 +0300

    build: update gradle wrapper and configure lint
    Include Gradle wrapper files, ignore keystore files, and disable strict linting for release builds to streamline the build process.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,10 @@ android {
     buildConfig = true
   }
   testOptions { unitTests { isIncludeAndroidResources = true } }
+  lint {
+    checkReleaseBuilds = false
+    abortOnError = false
+  }
   dependenciesInfo {
     includeInApk = false
     includeInBundle = true
